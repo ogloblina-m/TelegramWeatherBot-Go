3ccaa6977df067552f0ee15e0a17587ff3df8e0f
ogloblina-m <ogloblina.milena@gmail.com>
2023-05-26 06:38:52 +0500

    Added bot

---
+++ Telegram_bot.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"os"
+)
+
+func telegramBot() {
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, _ := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+	}
+}
